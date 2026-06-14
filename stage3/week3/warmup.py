62e8feb7f405219d650435bbd259aa58b749eff2
Youngwon Lee <yw0781@gmail.com>
2026-06-14 18:16:39 -0400

    Remove Stage 3 files from tracking

--- stage3/week3/warmup.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-df=pd.read_csv("Stage3/GDSC2-dataset.csv")
-
-df_venet=df[df["DRUG_NAME"]=="Venetoclax"].copy()
-df_venet_group=df_venet.groupby("TCGA_DESC")["LN_IC50"].mean().reset_index()
-top5=df_venet_group.sort_values("LN_IC50").head(5)
-print(top5)
